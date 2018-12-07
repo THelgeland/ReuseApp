512eca79a03cdd69e284d5558fef7b612cd3fa5e
THelgeland <torkil.helgeland@gmail.com>
2018-12-07 13:04:57 +0100

    gps
    Signed-off-by: THelgeland <torkil.helgeland@gmail.com>

--- app/src/main/java/dk/reuseapp/reuseapp/ResultActivity.java
+++ app/src/main/java/dk/reuseapp/reuseapp/ResultActivity.java
@@ -1,9 +1,12 @@
 package dk.reuseapp.reuseapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentActivity.*;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,18 +19,31 @@ import com.google.android.gms.maps.SupportMapFragment;
 
 public class ResultActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap map;
+    private PostInfo post;
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.post_container);
+
+        Intent intent = getIntent();
+        post = (PostInfo) intent.getSerializableExtra("PostInfo");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.googleMapsID);
         mapFragment.getMapAsync(this);
+        TextView titleView = findViewById(R.id.postcontainerTitleID);
+        titleView.setText(post.getTitle());
+        TextView descView = findViewById(R.id.postcontainerDescriptionID);
+        descView.setText(post.getDescription());
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
 
+        Location postLocation = Util.parseLocation(post.getLocation());
+        LatLng postLatLng = new LatLng(postLocation.getLatitude(), postLocation.getLongitude());
+        map.addMarker(new MarkerOptions().position(postLatLng).title("Post Position"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(postLatLng,17.0f));
     }
 }
